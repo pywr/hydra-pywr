b39ccf6c5245a297fa901f7a2f6bb759837c12c3
James Tomlinson <tomo.bbe@gmail.com>
2019-09-11 10:45:01 +0100

    Update template name to remove pywr.__git_hash__

--- hydra_pywr/template.py
+++ hydra_pywr/template.py
@@ -57,8 +57,7 @@ class TemplateExistsError(ValueError):
 
 def pywr_template_name(config_name):
     """ The name of the Hydra template for Pywr. """
-    return 'Pywr {} template (version: {}, git hash: {})'.format(config_name, pywr.__version__,
-                                                                 pywr.__git_hash__[:6])
+    return 'Pywr {} template (version: {})'.format(config_name, pywr.__version__)
 
 
 def generate_pywr_attributes():
