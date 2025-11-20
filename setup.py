9bdbbd21191f96bd059412143d0126862b478965
Stephen Knox <stephen.knox@manchester.ac.uk>
2025-11-20 12:11:30 +0000

    Use pyproject.toml instead of setup.py

--- setup.py
+++ setup.py
@@ -1,18 +1,5 @@
 #!/usr/bin/env python
-from setuptools import setup, find_packages
+from setuptools import setup
 
-setup(
-    name='hydra-pywr',
-    version='0.1',
-    description='Hydra plugins and library for import and exporting Pywr models.',
-    packages=find_packages(),
-    package_data={
-        # Include all template config files
-        'hydra_pywr': ['template_configs/*.json'],
-    },
-    include_package_data=True,
-    entry_points='''
-    [console_scripts]
-    hydra-pywr=hydra_pywr.cli:start_cli
-    ''',
-)
+# Configuration is in pyproject.toml
+setup()
